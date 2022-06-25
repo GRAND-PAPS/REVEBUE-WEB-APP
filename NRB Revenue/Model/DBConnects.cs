995aaf2b7596fdf48cadf173092ef23280dfbda2
Isaac SB Mkwehiwah <Grandpaps@outlook.com>
2022-06-25 16:21:40 +0200

    DBConnection; Reoprt and Settings pages Have been modified

--- NRB Revenue/Model/DBConnects.cs
+++ NRB Revenue/Model/DBConnects.cs
@@ -13,5 +13,11 @@ namespace NRB_Revenue.Model
             string connection = ConfigurationManager.ConnectionStrings["Development"].ToString();
             return connection;
         }
+
+        public static string GetconnRev()
+        {
+            string conn = ConfigurationManager.ConnectionStrings["RevDevo"].ToString();
+            return conn;
+        }
     }
 }
